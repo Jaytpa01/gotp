aeaf8eca93e0108c7478bfd3b2ada2960b13b3c0
Jay <jaytpa01@gmail.com>
2024-08-17 18:32:33 +1000

    refactor(otp): remove unnecessary interfaces

--- otp.go
+++ otp.go
@@ -3,23 +3,9 @@ package gotp
 import "hash"
 
 type OTP struct {
-	algorithm        OTPer
 	hashingAlgorithm func() hash.Hash
 	secret           []byte
 
 	issuer string
 	label  string
-}
-
-type OTPer interface {
-	Generator
-	Verifier
-}
-
-type Generator interface {
-	Generate() string
-}
-
-type Verifier interface {
-	Verify(string) bool
 }
