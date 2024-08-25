55502f9ea3dc13f4d4a6386dbe852c220dad4054
Jay <jaytpa01@gmail.com>
2024-08-25 19:22:37 +1000

    feat(padding): add util to pad secrets if needed

--- totp/totp.go
+++ totp/totp.go
@@ -22,14 +22,8 @@ func New(hash func() hash.Hash, digits int, period int) *totp {
 	}
 }
 
-func (o *totp) validate() error {
-	if o.period <= 0 {
-		return errors.New("period must be greater than 0")
-	}
-
-	return nil
-}
-
+// Generate generates a TOTP (Time-based One-Time Password) code given
+// the shared secret and a time.
 func (o *totp) Generate(secret []byte, when time.Time) (string, error) {
 	err := o.validate()
 	if err != nil {
@@ -39,4 +33,37 @@ func (o *totp) Generate(secret []byte, when time.Time) (string, error) {
 	count := when.Unix() / int64(o.period)
 	token := hotp.New(o.hash, o.digits).Generate(secret, count)
 	return token, nil
+}
+
+// padSecret pads the secret byte slice by repeating the secret
+// until it is the desired length
+//
+// e.g:
+// padSecret([]byte("12345678901234567890"), 32)
+// is equal to []byte("12345678901234567890123456789012")
+func padSecret(secret []byte, minLength int) []byte {
+	secretLength := len(secret)
+
+	if secretLength >= minLength {
+		return secret
+	}
+
+	paddedSecret := make([]byte, minLength)
+	copy(paddedSecret, secret) // copy the secret into the padded secret
+
+	// fill the rest of the padded secret by repeating the
+	// supplied secret until it is the desired length
+	for i := secretLength; i < minLength; i++ {
+		paddedSecret[i] = secret[i%secretLength]
+	}
+
+	return paddedSecret
+}
+
+func (o *totp) validate() error {
+	if o.period <= 0 {
+		return errors.New("period must be greater than 0")
+	}
+
+	return nil
 }
